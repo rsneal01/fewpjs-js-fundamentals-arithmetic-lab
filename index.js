ea803a039b533192afb4e36fc64a7f4712dda64b
rsneal01 <rsneal01@gmail.com>
2021-10-01 09:10:21 -0700

    commit tree 8326a1f2292ee25788508ff345a1e06a40837edc

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const num3 = 25
+const num4 = 7
+
+const multiply = num1 * num2
+const random = Math.floor(Math.random()*100)
+const mod = num3 % num4
+const max = Math.max(1,2,3,20)
